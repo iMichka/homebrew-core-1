875e1ed8ec4a91af3fdd07ce1b3b01cd5b48fca5
Michka Popoff <michkapopoff@gmail.com>
2021-04-03 23:29:55 +0200

    gpredict: remove workaround

--- Formula/gpredict.rb
+++ Formula/gpredict.rb
@@ -33,9 +33,6 @@ class Gpredict < Formula
   uses_from_macos "curl"
 
   def install
-    # Needed by intltool (xml::parser)
-    ENV.prepend_path "PERL5LIB", "#{Formula["intltool"].libexec}/lib/perl5" unless OS.mac?
-
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make", "install"
